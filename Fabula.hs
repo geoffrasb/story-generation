6acb852bd422dbd505fe481056e8aff312a2bb31
Geoffr Su <geoffrasb@gmail.com>
2013-09-25 14:11:50 +0800

    updated to recursive Fabula and defined the interface

--- Fabula.hs
+++ Fabula.hs
@@ -10,101 +10,52 @@ import qualified Data.Map as Map
 import qualified Ddta.List as List
 import Utils
 
-data Event = 
-    Event {             -- the representation should be considered
-        name :: String,
-        characters :: [String]
+type Key = Int
+
+
+data EntityInfo = 
+    EntityInfo {
+        addr  :: [Key],   --[who i am, who contains me, .....]
+        next  :: [Entity],
+        prev  :: [Entity]
+    }
+    deriving(Eq,Ord,Show)
+
+data Fabula = 
+    SingleEvent {
+        evtPred :: Predicate
     } 
-    | AbsEvent {
-        name :: String,
-        characters :: [String],
-        innerFab :: Fabula
+    | Fabula {
+        evtPred    :: Predicate,
+        table      :: Map.Map Int Entity
+        keyCounter :: Key
     }
     deriving (Ord, Show)
 
-evtName :: Event -> String
-evtName (Event n _) = n
-evtName (AbsEvent n _ _) = n
-invoChar :: Event -> [String]
-invoChar (Event _ c) = c
-invoChar (AbsEvent _ c _) = c
+type Entity = (EntityInfo, Fabula)
 
-instance Eq Event where
-    a == b = (evtName a==evtName b) && (invoChar a==invoChar b)
-
-fromPred :: Predicate -> Event
-fromPred (n,_,c) = Event{name=n, characters=c}
-predOf :: Event -> Predicate
-predOf Event{name=n, characters=c} = (n, length c,c)
-
-data Plot = EmptyPlot | PlotPiece Event Plot
-
-
-
------- Fabula
-
-
-data EntityInfo = EntityInfo {
-    eid      :: Int, 
-    fid      :: [Int],
-    next     :: [Entity],
-    prev     :: [Entity]
-}deriving(Eq,Ord,Show)
-
-
-data Fabula = Fabula {
-    key     :: Int,
-    evtKeyCounter :: Int,
-    table   :: Map.Map Int (EntityInfo, Event)
-}deriving (Show)
+evtInfo :: Fabula -> Predicate
+evtInfo (SingleEvent p) = p
+evtInfo (Fabula p _ _) = p
 
 instance Eq Fabula where -- weak equality
-    Fabula{key=ka} == Fabula{key=kb} = ka==kb
-
-emptyFabula fabId= Fabula fabId 0 Map.empty
+    a == b = evtInfo a==evtInfo b
 
 
----------- Container
-
-data Container = Container {
-    key     :: Int,
-    fabKeyCounter :: Int,
-    table   :: Map.Map Int Fabula
-}
-
-emptyContainer = Container 0 Map.empty
-
-addNewFabula :: Container -> (Fabula,Container)
-addNewFabula Container{fabKeyCounter=c, table=t} = 
-    Container{fabKeyCounter=c+1, table=Map.insert c (emptyFabula c) t}
-    
-addFabula    :: Fabula -> Container -> Container --throw out the original key of the fabula
--- just as extract all fabula in the original fabula, change their keys and insert to the container
-addFabula Fabula{ evtKeyCounter=fekc, table=entityTable} Container{key=ck, fabKeyCounter=cfkc, table=fabulaTable} =
-    Container{ key=ck, fabKeyCounter=cfkc+1, table=newTable}
-    where
-        newTable = Map.insert cfkc (Fabula cfkc fekc keyChangedEntityTable) fabulaTable
-        keyChangedEntityTable = fmap () entityTable
-
-removeFabula :: Fabula -> Container -> Container
-removeFabula Fabula{key=k} c = removeFabulaWithKey k c
-removeFabulaWithKey :: Int -> Container -> Container
-removeFabulaWithKey k Container{keyCounter=c, table=t} =
-    Container{keyCounter=c, table=Map.delete t}
+--data Plot = EmptyPlot | PlotPiece Predicate Plot
+type Plot = [Fabula]
 
 
------- Operations
-
---select :: FabulaEntity -> Fabula -> Maybe FabulaEntity
---selectEvent :: Event -> Fabula -> [FabulaEntity]
-insert :: Event -> Fabula -> (Entity,Fabula)
-remove :: Entity -> Fabula -> Fabula
-addConseqOf :: Entity -> Fabula -> Either Entity Event -> Fabula
-addCauseOf  :: Entity -> Fabula -> Either Entity Event -> Fabula
+insert :: Fabula -> Fabula -> (Entity,Fabula)
+remove :: [Key] -> Fabula -> Fabula
+update :: Entity -> Fabula -> Fabula
+conseqLink :: (Entity, Entity) -> Fabula -> Fabula
+causeLink  :: (Entity, Entity) -> Fabula -> Fabula
 conseqOf :: Entity -> [Entity]
 causeOf  :: Entity -> [Entity]
 abstract   :: Entity -> Fabula -> (Entity,Fabula)
 deabstract :: Entity -> Fabula -> Fabula
+
 
 
 
