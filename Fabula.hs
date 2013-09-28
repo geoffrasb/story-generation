872c62a96ba4ce64e0d69eb7e9d286f32be31703
Geoffr Su <geoffrasb@gmail.com>
2013-09-28 13:57:55 +0800

    finish Fabula's interface

--- Fabula.hs
+++ Fabula.hs
@@ -23,13 +23,13 @@ keyGen = KeyGen bottomKey
 data EntityInfo = 
     EntityInfo {         
         key   :: Key,
-        infab :: Maybe Fabula,
+        inEnt :: Maybe Entity, -- in entity
         next  :: [Fabula],
         prev  :: [Fabula]
     }
     deriving(Ord,Show)
 instance Eq EntityInfo where
-    EntityInfo ka fa _ _ == EntityInfo kb fb _ _ = ka==kb && fa=fb
+    (EntityInfo ka fa _ _) == (EntityInfo kb fb _ _) = ka==kb && fa==fb
 
 data Fabula = 
     SingleEvent {
@@ -40,6 +40,13 @@ data Fabula =
         table      :: Map.Map Key Entity
     }
     deriving (Ord, Show)
+instance Eq Fabula where -- weak equality
+    a == b = evtInfo a==evtInfo b
+{-
+instance Functor Fabula where
+    fmap f e@(SingleEvent e) = f e
+    fmap f (Fabula evt t) = f $ Fabula evt (fmap f t)
+    -}
 
 type Entity = (EntityInfo, Fabula)
 
@@ -47,8 +54,6 @@ evtInfo :: Fabula -> Predicate
 evtInfo (SingleEvent p) = p
 evtInfo (Fabula p _) = p
 
-instance Eq Fabula where -- weak equality
-    a == b = evtInfo a==evtInfo b
 
 
 --data Plot = EmptyPlot | PlotPiece Predicate Plot
@@ -59,13 +64,58 @@ type Plot = [Fabula]
 
 -- Fabula construction: every fabula should get a unique ID
 newEntity :: Fabula -> KeyGen -> (Entity, KeyGen)
-newEntity fab kg = ((EntityInfo{ key=nkey, infab=Nothing, next=[], prev=[]}, fab),
+newEntity fab kg = ((EntityInfo{ key=nkey, inEnt=Nothing, next=[], prev=[]}, fab),
                     newkg)
                    where
                     (nkey,newkg) = getKey kg
 
-moveIn  :: Entity -> Entity -> Entity
-moveOut :: Entity -> Entity -> Entity
+
+{-
+    operations below may be constructed from fmap...?
+
+
+-}
+
+
+attach  :: Entity -> Entity -> Maybe Entity
+-- Tag that ent1 is wrapped in ent2. 
+--  If ent1 is already wrapped in an entity, then it comes out with Nothing.
+--  If not, it returns the new ent1.
+attach ((EntityInfo _ (Just _) _ _), _) _ = Nothing -- means ent1 is already a child of an entity.
+attach ((EntityInfo key1 Nothing n1 p1), fab1) ent2 =
+    Just ((EntityInfo key1 (Just ent2) n1 p1), fab1)
+
+
+detach :: Entity -> (Entity, Maybe Entity)
+-- Free the ent1.
+--  If ent1 is wrapped in ent2, then it returns (ent1 without parent, ent2 without ent1)
+--  If ent1 has no parent, then (original ent1, Nothing)
+detach ent1@((EntityInfo _ Nothing _ _), _) = (ent1, Nothing)
+detach ((EntityInfo key1 (Just inEnt1) n1 p1), fab1) =
+    (((EntityInfo key1 Nothing n1 p1), fab1), Just newOutterEnt)
+    where
+        newOutterEnt = case inEnt1 of
+                        (_, (SingleEvent _)) -> 
+                            error "Error: in detach, the first argument attached to a SingleEvent."
+                        (ei, (Fabula evt table)) ->
+                            (ei, Fabula evt $ Map.delete key1 table)
+
+
+updateOutterEntity :: Entity -> Maybe Entity
+-- Returns the updated entity which used to have the key that links to 
+--      the old version of the input entity.
+--  Nothing if the entity has no parent.
+updateOutterEntity ent1@((EntityInfo key1 inEnt1 n1 p1), fab1) = 
+    case inEnt1 of
+        Nothing -> Nothing
+        Just (ei2, fab2) -> Just (ei2, updatedFab2)
+            where
+                updatedFab2 = case fab2 of
+                                --inEnt1 will definitely be updated with ent1
+                                (SingleEvent evt) -> 
+                                    Fabula evt $ Map.fromList [(key1, ent1)]
+                                (Fabula evt table) ->
+                                    Fabula evt $ Map.alter (\_->Just ent1) key1 table
 
 
 
